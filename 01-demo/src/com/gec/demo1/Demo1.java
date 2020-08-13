ff1d6ad4200e20febcfb0af2ecc08794e85752dc
ximenqing <123@qq.com>
2020-08-13 22:12:04 +0800

    xxx

--- 01-demo/src/com/gec/demo1/Demo1.java
+++ 01-demo/src/com/gec/demo1/Demo1.java
@@ -3,16 +3,6 @@ package com.gec.demo1;
 public interface Demo1 {
 
 	void addUser();
-	
-	// 西门庆又加了一个方法
-	void hellworld();
-	
-	// 张三提交了aa 
-	void aaa();
-	
-	// 李四提交了 lisi()
-	void lisi();
-	
-	void ddd();
+
 	
 }
